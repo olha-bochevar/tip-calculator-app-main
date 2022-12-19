c0e97dddaf3316546903a49b7375373a77f72114
Olha Bochevar <olya.bochevarland@gmail.com>
2022-12-19 22:25:55 +0200

    Add functionality of tip calculating & showing results with input events onclick. Add css rules and changes of DOM for custom tip input from type radio to number. Add functionality reset button.

--- script.js
+++ script.js
@@ -1,80 +1,127 @@
-// DOM const
-const form = document.querySelector('.form');
-const btnReset = form.querySelector('.btn-reset');
-let bill = form.bill;
-let numberOfPeople = form.people;
-const tips = document.querySelectorAll('div.select-tip-input > fieldset > div.tips > div.tip');
-const customTip = document.querySelector("div.select-tip-input > fieldset > div > div.tip.custom > input#val-custom");
-const inputCustomNumber = document.querySelector('#val-custom-number');
-const outputTipAmount = form.querySelector('#tip-amount span.value');
-const outputTotal = form.querySelector('#total span.value');
-const warn = form.querySelector('.warn');
+// DOM
 
-let tip = 15;
+// main
+const form = document.querySelector(".form");
+const btnReset = form.querySelector(".btn-reset");
+// inputs
+const inputBill = form.querySelector("input#bill");
+const inputNumOfPeople = form.querySelector("input#people");
+const tips = document.querySelectorAll(".tip input");
+const customTip = document.querySelector("input#val-custom");
+const inputCustomTip = document.querySelector("#val-custom-number");
+// output fields
+const outputTipAmount = form.querySelector("#tip-amount span.value");
+const outputTotal = form.querySelector("#total span.value");
+const warn = form.querySelector(".warn");
 
-// reset values
+
+
+// FUNCTIONS
+
+// reset values for inputs
 const resetValues = (input) => {
-    input.value = '';
+  input.value = "";
 };
-
-bill.addEventListener('click', () => {
-    resetValues(bill);
+inputBill.addEventListener("click", () => {
+  resetValues(inputBill);
 });
-numberOfPeople.addEventListener('click', () => {
-    resetValues(numberOfPeople);
+inputNumOfPeople.addEventListener("click", () => {
+  resetValues(inputNumOfPeople);
+});
+inputCustomTip.addEventListener("click", () => {
+  resetValues(inputCustomTip);
 });
 
 // reset form by clicking button 'Reset'
-btnReset.addEventListener('click', e => { 
-    form.reset();
-    outputTipAmount.textContent = `$0.00`;
-    outputTotal.textContent = `$0.00`;
+btnReset.addEventListener("click", (e) => {
+  form.reset();
+  showCustomRadio();
+  hideWarn();
+  outputTipAmount.textContent = `$0.00`;
+  outputTotal.textContent = `$0.00`;
 });
 
 // math operations
-
-const getTipAmountPerPerson = (bill, tip, numberOfPeople) => {
-    const sumTipAllAmount = (bill*tip) / 100;
-    return sumTipAllAmount / numberOfPeople;
+const getTipAmountPerPerson = (bill, tip, numOfPeople) => {
+  const sumTipAllAmount = (bill * tip) / 100;
+  return sumTipAllAmount / numOfPeople;
 };
-const getTotalSumPerPerson = (bill, tip, numberOfPeople) => {
-    const sumTipAllAmount = (bill*tip) / 100;
-    const sumTotal = bill + sumTipAllAmount;
-    return sumTotal / numberOfPeople;
+const getTotalSumPerPerson = (bill, tip, numOfPeople) => {
+  const sumTipAllAmount = (bill * tip) / 100;
+  const sumTotal = bill + sumTipAllAmount;
+  return sumTotal / numOfPeople;
 };
 
-// update UI 
+// update UI
 const updateUI = () => {
-    const sumTipAmountPerPerson = getTipAmountPerPerson(bill, tip, numberOfPeople).toFixed(2);
-    const sumTotalPerPerson = getTotalSumPerPerson(bill, tip, numberOfPeople).toFixed(2);
+  // get values bill & number of person
+  let bill = Number(form.bill.value.trim());
+let numberOfPeople = Number(form.people.value.trim());
 
+  // get tip
+  let tip;
+  tips.forEach((el) => {
+    if (el.checked) {
+      tip = el.value;
+      showCustomRadio();
+    } else if (customTip.checked) {
+      hideCustomRadio();
+      tip = inputCustomTip.value;
+    }
+  });
+
+  // check number of people & output message about mistake
+  if (numberOfPeople < 1) {
+   showWarn();
+  } else {
+    hideWarn();
+  }; 
+
+  // get & output results
+  const sumTipAmountPerPerson = getTipAmountPerPerson(
+    bill,
+    tip,
+    numberOfPeople
+  ).toFixed(2);
+  const sumTotalPerPerson = getTotalSumPerPerson(
+    bill,
+    tip,
+    numberOfPeople
+  ).toFixed(2);
+  // check for type results (do not show 'Nan' and 'Infinity', if any input hasn`t filled yet)
+  if (bill > 0 && numberOfPeople > 0 && tip > 0) {
     outputTipAmount.textContent = `$${sumTipAmountPerPerson}`;
     outputTotal.textContent = `$${sumTotalPerPerson}`;
-
+  } else {
+    outputTipAmount.textContent = `$0.00`;
+    outputTotal.textContent = `$0.00`;
+  }
 };
-
-// get values from form && update UI
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    
-    bill = Number(form.bill.value.trim());
-    numberOfPeople = Number(form.people.value.trim());
-    updateUI();
-
-    
-});
 
 // change custom input 'radio' to 'number'
+function hideCustomRadio() {
+  if (customTip.checked) {
+    customTip.classList.add("hidden");
+    customTip.nextElementSibling.setAttribute("class", "hidden");
+    inputCustomTip.classList.remove("hidden");
+    inputCustomTip.classList.add("input-tip-custom");
+  }
+}
+function showCustomRadio() {
+  if (!customTip.checked) {
+    customTip.classList.remove("hidden");
+    customTip.nextElementSibling.setAttribute("class", "");
+    inputCustomTip.classList.remove("input-tip-custom");
+    inputCustomTip.classList.add("hidden");
+  }
+}
 
-function hide() {
-    if(customTip.checked) {
-        customTip.classList.add('hidden');
-        customTip.nextElementSibling.setAttribute('class', 'hidden');
-        inputCustomNumber.classList.remove('hidden');
-        inputCustomNumber.classList.add('input-tip-custom');
-    } else {
-        customTip.nextElementSibling.setAttribute('class', '');
-    }
+function showWarn() {
+    warn.textContent = `Can't be zero`;
+    inputNumOfPeople.classList.add("warn-focused");
 };
 
-
+function hideWarn() {
+    warn.textContent = "";
+    inputNumOfPeople.classList.remove("warn-focused");
+};
